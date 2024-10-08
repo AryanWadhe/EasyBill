3bdb489291da2e9fb3975b2e4e4a5612f4fb9a77
AryanWadhe <96442442+AryanWadhe@users.noreply.github.com>
2024-10-08 15:31:30 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,52 +8,42 @@ import authRouter from "./routes/auth.router.js";
 import invoiceRouter from "./routes/invoice.router.js";
 import analyticsRouter from "./routes/analytics.router.js";
 import PDFDocument from "pdfkit-table";
-
+console.log("first0")
 const app = express();
 dotenv.config();
 
-// Define allowed origins based on the environment
-const allowedOrigins = [
-  // "http://localhost:3000", // Local development frontend
-  "https://quick-bill-client.vercel.app", // Vercel frontend
-];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps, curl, etc.)
-      if (!origin) return callback(null, true);
+const corsOptions = {
+  origin: 'https://invo-sync.vercel.app', // allow requests from your frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // allowed methods
+  credentials: true, // allow cookies or authentication headers
+  optionsSuccessStatus: 200 // some legacy browsers choke on 204
+};
 
-      // Check if the incoming request's origin is in the allowed list
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // Allow cookies and credentials
-  })
-);
+app.use(cors(corsOptions));
 
+// For preflight requests
+app.options('*', cors(corsOptions));
+console.log("first")
 app.use(express.json());
 app.use(cookieParser());
 
-// Connect to the database
 connectDB();
 
-// Define your routes
-app.get("/", (req, res) => {
-  res.send("Welcome to Quick Bill API");
-});
 
-app.use("/data", randomDataRouter);
-app.use("/auth", authRouter);
-app.use("/analytics", analyticsRouter);
-app.use("/invoice", invoiceRouter);
+const port = process.env.PORT;
 
-// Start the server
-const port = process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+  res.send({
+    api:"your backend is running"
+  })
+})
+
+app.use("/data",cors(), randomDataRouter);
+app.use("/auth",cors(), authRouter);
+app.use("/analytics",cors(), analyticsRouter);
+app.use("/invoice",cors(), invoiceRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
